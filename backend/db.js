03fc4bfe38e5dc815f6b8e5bf4603b4a577d4e6d
Lourenço Viguini Werneck <lourencovw@gmail.com>
2026-01-15 18:51:17 -0300

    feat: migrate database files to typescript

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-const knex = require("knex");
-const config = require("./knexfile");
-
-const db = knex(config.development);
-
-// BUG: no pooling limits, no error handling, reused connection not validated.
-module.exports = db;
